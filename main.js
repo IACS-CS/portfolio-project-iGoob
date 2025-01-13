eb71f1b991250778480a4cc9e7070599d1e75667
iGoob <joseph.burns@innovationcharter.org>
2025-01-13 19:34:05 +0000

    test

--- main.js
+++ main.js
@@ -1,24 +1,2 @@
 // main.js
 
-// List of image sources (placeholders)
-const placeholderImages = [
-  "placeholder1.png", // Placeholder 1
-  "placeholder2.png", // Placeholder 2
-  "placeholder3.png", // Placeholder 3
-];
-
-// Reference to the clickable item
-const clickableItem = document.getElementById("clickable-placeholder");
-
-// Reference to the image inside the clickable item
-const imageElement = clickableItem.querySelector("img");
-
-// Current image index
-let currentIndex = 0;
-
-// Click event listener to cycle through images
-clickableItem.addEventListener("click", function () {
-  // Change to the next image
-  currentIndex = (currentIndex + 1) % placeholderImages.length;
-  imageElement.src = placeholderImages[currentIndex]; // Update image source
-});
